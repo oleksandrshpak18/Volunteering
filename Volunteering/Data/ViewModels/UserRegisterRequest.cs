342dbbd4f8e8561cf07d3dbe8cc2c084a64c26ba
Oleksandr Shpak <oleksandr.o.shpak@gmail.com>
2024-06-12 18:36:36 +0300

    hotfix

--- Volunteering/Data/ViewModels/UserRegisterRequest.cs
+++ Volunteering/Data/ViewModels/UserRegisterRequest.cs
@@ -14,7 +14,7 @@ namespace Volunteering.Data.ViewModels
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; } = null!;
         [Required]
-        [RegularExpression(@"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)", ErrorMessage = "Пароль повинен містити щонайменше 1 велику літеру, 1 малу літерута 1 цифру")]
+        //[RegularExpression(@"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)", ErrorMessage = "Пароль повинен містити щонайменше 1 велику літеру, 1 малу літерута 1 цифру")]
         public string Password { get; set; } = null!;
     }
 }
